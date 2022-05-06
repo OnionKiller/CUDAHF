f6355c26a27a0b495335db7c3766cdfd57c35575
OnionKiller <tomi@csilling.com>
2022-05-06 17:06:27 +0200

    fix block optimisation related issues, sum works now perfectly

--- kernel.cu
+++ kernel.cu
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <iostream>
 #include <random>
+#include <algorithm>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-cudaError_t scanWithCuda(int* a, const int* b, unsigned int size);
+cudaError_t scanWithCuda(int * ret, const int * in, unsigned int size);
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
@@ -88,7 +89,7 @@ __global__ void scanPartialResults(int* sum,int* data) {
     }
 
     //add cumulative sum
-    sum[i] += s[1023];
+    sum[i] = data[i] + s[1023];
     __syncthreads();
 }
 
@@ -102,26 +103,34 @@ int main()
     auto gen = std::mt19937(rd());
     auto distribution = std::binomial_distribution<int>(1023, 1. / 128.);
 
-    for (auto i = 0;i<arraySize;i++)
-        i = 1;
+    std::generate(a, a + arraySize, [&]() {
+        return 1;
+        });
 
     // Add vectors in parallel.
-    cudaError_t cudaStatus = scanWithCuda(a, b, arraySize);
+    cudaError_t cudaStatus = scanWithCuda(b, a, arraySize);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "addWithCuda failed!");
         return 1;
     }
 
-//    for (auto i : b)
-//        std::cout << i << ',';
-    std::cout << "test full sum";
+    std::cout << "test full sum" << std::endl;
+    for (auto i = 2025; i < 2050; i++)
+        std::cout << a[i] << ',';
     std::cout<<std::endl;
     for (auto i = 10; i-- > 0;)
         std::cout << '-';
     std::cout << std::endl;
-//    for (auto i : a)
-//        std::cout << i << ',';
-    std::cout << " Full sum is: " << b[1024 * 1024 - 1] << " expected: " << 1024 * 1024;
+    for (auto j = 1; j < 1024; j++)
+    {
+        if(b[j*1024]-b[j*1024-1] != 1)
+        {
+            std::cout << std::endl << "error at:" << j << std::endl;
+        for(auto i = j*1024-5;i<j*1024+5;i++)
+            std::cout << b[i] << ',';
+        }
+    }
+    std::cout << std::endl << " Full sum is: " << b[1024 * 1024 - 1] << " expected: " << 1024 * 1024;
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -135,10 +144,10 @@ int main()
 }
 
 // Helper function for using CUDA to add vectors in parallel.
-cudaError_t scanWithCuda(int* a, const int* b, unsigned int size)
+cudaError_t scanWithCuda(int * ret, const int * in, unsigned int size)
 {
-    int* dev_a = 0;
-    int* dev_b = 0;
+    int* dev_ret = 0;
+    int* dev_in = 0;
     cudaError_t cudaStatus;
 
     // Choose which GPU to run on, change this on a multi-GPU system.
@@ -148,29 +157,29 @@ cudaError_t scanWithCuda(int* a, const int* b, unsigned int size)
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_ret, size * sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_in, size * sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
     // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaStatus = cudaMemcpy(dev_in, in, size * sizeof(int), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
     // Launch a kernel on the GPU with one thread for each element.
-    scanKernel <<<4, 1024 >> > (dev_a, dev_b);
+    scanKernel <<<1024, 1024 >> > (dev_ret, dev_in);
     // sync blocks
     // sum over blocks
-    scanPartialResults<<<4, 1024>>>(dev_a,dev_a);
+    scanPartialResults<<<1024, 1024>>>(dev_in, dev_ret);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
@@ -188,15 +197,15 @@ cudaError_t scanWithCuda(int* a, const int* b, unsigned int size)
     }
 
     // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(a, dev_a, size * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(ret, dev_in, size * sizeof(int), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
 Error:
-    cudaFree(dev_a);
-    cudaFree(dev_b);
+    cudaFree(dev_ret);
+    cudaFree(dev_in);
 
     return cudaStatus;
 }
